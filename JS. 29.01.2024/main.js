d95f1e368f392902883069c0354bc45d72b8a515
Offite <144914863+Offite@users.noreply.github.com>
2024-01-29 14:16:53 +0300

    Add files via upload

---
+++ JS. 29.01.2024/main.js
@@ -0,0 +1,23 @@
+// let arr = ['Привет, ', 'мир', '!'];
+// console.log(arr.join(''));
+
+// let arr = ['Привет, ', 'мир', '!'];
+// let text = arr.join('');
+// console.log(text);
+
+// let arr = ['Привет, ', 'мир', '!'];
+// arr[0] = 'Пока, ';
+// console.log(arr.join(''));
+
+// let obj = {
+//     Петя: 3000,
+//     Коля: 4000
+//   };
+//   console.log(obj.Петя);
+//   console.log(obj.Коля);
+
+// let arr = {
+//   'ru':['голубой', 'красный', 'зелёный'],
+//   'en':['blue', 'red', 'green']
+// };
+// console.log(arr.ru[1]);
